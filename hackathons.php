b095a191fb88ebef2895696527c5b37f70262590
Brandon Geraldo <Brandongeraldo@student.ub.ac.id>
2025-05-23 18:58:59 +0700

    Ganti footer samain index di includes footer.php

--- hackathons.php
+++ hackathons.php
@@ -48,17 +48,18 @@ if ($needsPatch) {
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Hack.id - Find Your Team. Hack the Future </title>
+    <title>Hack.id - Find Your Team</title>
     <link rel="Website Icon" type="png" href="img/Logo1.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Google Fonts & Icons -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
       rel="stylesheet"
-    >
+    />
+    <link
     
     <link rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
